9e39db5e7bc09cf98be9c4bc2a38c4f31b5c521e
Murtaza <murtazahotaki@yahoo.com>
2022-07-30 23:42:39 +0500

    Upto Date

---
+++ app/src/main/java/com/festdepartment/awaaz_e_bezuban/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.festdepartment.awaaz_e_bezuban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
